4151776b59636d21d8cdf837618584d166ce27f6
Johan Peltenburg <johan.peltenburg+code@gmail.com>
2026-05-29 13:08:43 +0200

    Add schema and convention crates

---
+++ crates/schema/src/event.rs
@@ -0,0 +1,41 @@
+// SPDX-FileCopyrightText: Copyright (c) 2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+use crate::{convention::Convention, data_type::DataType, identifier::Identifier};
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+pub enum Cardinality {
+    /// The event can be emitted zero or one time.
+    Once,
+    /// The event can be emitted zero or multiple times.
+    Multi,
+}
+
+#[derive(Clone, Debug, PartialEq)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+pub struct EventField {
+    /// The name of the event field.
+    pub name: Identifier,
+    /// Potential documentation.
+    pub docs: Option<String>,
+    /// The type of the event field.
+    pub ty: DataType,
+    /// Convention-specific metadata attached to this event field.
+    pub conventions: Vec<Convention>,
+}
+
+#[derive(Clone, Debug, PartialEq)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+pub struct Event {
+    /// The name of the event.
+    pub name: Identifier,
+    /// Potential documentation.
+    pub docs: Option<String>,
+    /// The [`Cardinality`] of the event.
+    pub cardinality: Cardinality,
+    /// The fields of the event.
+    pub payload: Vec<EventField>,
+    /// Convention-specific metadata attached to this event.
+    pub conventions: Vec<Convention>,
+}
